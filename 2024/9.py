c427c9287bdc43f0e4b0b4146ed97b7aedd66e54
20191663 <ihs20191663@gmail.com>
2024-12-05 14:45:56 +0900

    241205

---
+++ 2024/9.py
@@ -0,0 +1,50 @@
+import sys, math
+from collections import deque, defaultdict, Counter
+from itertools import permutations, combinations, product
+from bisect import bisect_left, bisect_right
+from copy import deepcopy
+from fractions import Fraction
+from decimal import *
+from heapq import heapify, heappop, heappush, heappushpop
+from datetime import datetime
+from functools import cache
+input = sys.stdin.readline
+f=open("1205.in","r")
+
+MI = lambda: map(int, f.readline().split())
+LI = lambda: list(map(int, f.readline().split()))
+II = lambda: int(f.readline())
+IR = lambda: f.readline().rstrip()
+LIR = lambda: list(f.readline().rstrip())
+LIRS = lambda: list(f.readline().rstrip().split())
+INF = math.inf
+
+x=False
+ans=0
+order=defaultdict(set)
+seq=[]
+while True:
+    IN=IR()
+    if not IN:
+        if not x:
+            x=True
+        else:
+            break
+        continue
+    if not x:
+        a,b=map(int,IN.split('|'))
+        order[b].add(a)
+    else:
+        seq.append(list(map(int,IN.split(','))))
+
+for s in seq:
+    temp=set([])
+    for i in s:
+        if i in temp:
+            break
+        else:
+            temp|=order[i]
+    else:
+        ans+=s[len(s)//2]
+print(ans)
+f.close()
