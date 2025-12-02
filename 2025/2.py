2a6abe0a1fe65ada6b6ab94e2938684824b34586
easter423 <ihs20191663@gmail.com>
2025-12-02 14:28:15 +0900

    251202

---
+++ 2025/2.py
@@ -0,0 +1,44 @@
+import sys, math
+from collections import deque, defaultdict, Counter
+from itertools import permutations, combinations, product
+from bisect import bisect_left, bisect_right
+from copy import deepcopy
+from fractions import Fraction
+from decimal import *
+from heapq import heapify, heappop, heappush, heappushpop
+from datetime import datetime
+from functools import cache
+f=open("1201.in","r")
+MI = lambda: map(int, f.readline().split())
+LI = lambda: list(map(int, f.readline().split()))
+II = lambda: int(f.readline())
+IR = lambda: f.readline().rstrip()
+LIR = lambda: list(f.readline().rstrip())
+LIRS = lambda: list(f.readline().rstrip().split())
+INF = math.inf
+# sys.setrecursionlimit(3*(10**5))
+# getcontext().prec=100
+
+x=50
+ans=0
+while True:
+    try:
+        a=IR()
+        b=int(a[1:])
+        if a[0]=='L':
+            for _ in range(b):
+                x-=1
+                if x<0:
+                    x+=100
+                elif x==0:
+                    ans+=1
+        else:
+            for _ in range(b):
+                x+=1
+                if x>=100:
+                    x-=100
+                if x==0:
+                    ans+=1
+    except:
+        break
+print(ans)
---
+++ 2025/3.py
@@ -0,0 +1,39 @@
+import sys, math
+from collections import deque, defaultdict, Counter
+from itertools import permutations, combinations, product
+from bisect import bisect_left, bisect_right
+from copy import deepcopy
+from fractions import Fraction
+from decimal import *
+from heapq import heapify, heappop, heappush, heappushpop
+from datetime import datetime
+from functools import cache
+f=open("1202.in","r")
+MI = lambda: map(int, f.readline().split())
+LI = lambda: list(map(int, f.readline().split()))
+II = lambda: int(f.readline())
+IR = lambda: f.readline().rstrip()
+LIR = lambda: list(f.readline().rstrip())
+LIRS = lambda: list(f.readline().rstrip().split(','))
+INF = math.inf
+# sys.setrecursionlimit(3*(10**5))
+# getcontext().prec=100
+
+x=[]
+for i in range(1,1000000):
+    x.append(int(str(i)+str(i)))
+
+ans=0
+while True:
+    try:
+        IN=LIRS()
+        for i in IN:
+            s,e=map(int,i.split('-'))
+            idx=bisect_left(x,s)
+            while idx<len(x) and x[idx]<=e:
+                if x[idx]>=s:
+                    ans+=x[idx]
+                idx+=1
+    except:
+        break
+print(ans)
